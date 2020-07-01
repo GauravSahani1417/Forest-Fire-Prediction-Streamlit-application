ede08aa06d4355777dbd53e93edfbe5d8434b240
Gaurav Sahani <63406916+GauravSahani1417@users.noreply.github.com>
2020-07-01 14:46:03 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"gauravsahani1417@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
